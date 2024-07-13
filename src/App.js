0b26563e3abe5b5f92e4d6ef59a8c0170771bdc1
Ayden Smith <s_ayden@ymail.com>
2024-07-13 16:15:53 +1000

    Added particle system

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Skills from "./components/Skills/skills";
-import Projects from "./components/Projects/projects";
-import About from "./components/About/about";
+import { HashRouter } from "react-router-dom";
 import Navbar from "./components/Nav/navbar";
-import Home from "./components/Home/home";
 import Contact from "./components/Contact/contact";
 import Terminal from "./components/Terminal/terminal";
+import Particles from "./components/particles";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Particles id="tsparticles" />
+
       {/* Header bar containing contact icons */}
       <header>
         <Contact />
@@ -18,28 +17,11 @@ function App() {
 
       <div className="main">
         <HashRouter>
+          {/* Side nav */}
           <Navbar />
 
-          {/* Content of slide */}
-          <div className="content">
-            <Routes>
-              <Route exact path="/" element={<Home />}>
-                Home
-              </Route>
-
-              <Route exact path="/about" element={<About />}>
-                About
-              </Route>
-
-              <Route exact path="/projects" element={<Projects />}>
-                projects
-              </Route>
-
-              <Route exact path="/skills" element={<Skills />}>
-                Skills
-              </Route>
-            </Routes>
-          </div>
+          {/* Content terminal */}
+          <Terminal />
         </HashRouter>
       </div>
     </div>
--- src/components/Terminal/terminal.js
+++ src/components/Terminal/terminal.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Skills from "../Skills/skills";
+import Projects from "../Projects/projects";
+import About from "../About/about";
+import Home from "../Home/home";
+import "./terminal.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faWindowMinimize } from "@fortawesome/free-regular-svg-icons";
 
 function Terminal() {
-  return <div>terminal</div>;
+  return (
+    <div className="content">
+      <div id="terminal-tab">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/projects" element={<Projects />}></Route>
+          <Route exact path="/skills" element={<Skills />}></Route>
+        </Routes>
+      </div>
+
+      <div id="stdin">
+        <span>
+          C:\User\Guest <FontAwesomeIcon icon={faChevronRight} />
+          <span className="text">
+            <FontAwesomeIcon icon={faWindowMinimize} />{" "}
+          </span>
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Terminal;
